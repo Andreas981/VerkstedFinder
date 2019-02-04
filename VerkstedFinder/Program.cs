eab70cda4ce36cd8c7f6842a6f13c66fb50aa324
Andreas Mikalsen <amikal@hotmail.no>
2019-02-04 09:55:35 +0100

    Commit from VS

--- VerkstedFinder/Program.cs
+++ VerkstedFinder/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Console;
 
 namespace VerkstedFinder
 {
@@ -6,7 +7,8 @@ namespace VerkstedFinder
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            WriteLine("Hello World!");
+            WriteLine("HEY");
         }
     }
 }
