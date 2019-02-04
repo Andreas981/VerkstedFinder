f6b7a87ffa10399ead4d86b8ba0ec05c746827f8
Andreas Mikalsen <amikal@hotmail.no>
2019-02-04 09:48:51 +0100

    Initialized SLN

---
+++ VerkstedFinder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VerkstedFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
